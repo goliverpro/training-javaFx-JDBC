8771e743b35aa3f19dbb9291292a7e68606ed802
Gabriel Oliveira <gosilva.pro@gmail.com>
2020-11-24 22:51:44 -0300

    Saving Seller

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -71,6 +71,10 @@ public class Seller implements Serializable {
 		return baseSalary;
 	}
 
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
